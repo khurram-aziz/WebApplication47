d4b9120a364da341357d11c3b53a7f7056b30df8
khurram <khuziz@hotmail.com>
2017-11-22 16:21:07 +0500

    Added AjaxController and UserModel

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Linq;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class AjaxController : ApiController, IDisposable
+    {
+        IContractResolver _resolver;
+
+        public AjaxController()
+        {
+            var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            _resolver = formatter.SerializerSettings.ContractResolver;
+            formatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+
+        void IDisposable.Dispose()
+        {
+            var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            formatter.SerializerSettings.ContractResolver = _resolver;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Signup(UserModel user)
+        {
+            if (this.ModelState.IsValid)
+            {
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            else
+            {
+                return this.Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class UserModel
+    {
+        [MinLength(2)]
+        [MaxLength(10)]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please supply your email address.")]
+        public string EmailAddress { get; set; }
+
+        [Range(1, 100)]
+        public int Age { get; set; }
+        public string Location { get; set; }
+
+        [Required]
+        public string Subscription { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
