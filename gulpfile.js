971678aa333c2c207198119a2b7ac9bc93f935d8
khurram <khuziz@hotmail.com>
2017-12-24 09:25:18 +0500

    hello-vuetify

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding BeforeBuild='copy-files' />
+﻿/// <binding BeforeBuild='default' />
 /*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
@@ -16,42 +16,43 @@ var fs = require('fs');
 var production = (process.env.NODE_ENV === 'production');
 
 gulp.task('default', function () {
-    // place code for your default task here
+    gUtil.log(production ? 'NODE_ENV is production' : 'NODE_ENV is not production');
+    gulp.start('vue-task');
 });
 
-gulp.task('copy-files', function () {
-    gUtil.log(production ? 'NODE_ENV is production' : 'NODE_ENV is not production');
+gulp.task('vue-task', function () {
+    const vueVendors = ['vue', 'vue-router', 'vuetify'];
 
-    //vue
-    gulp.src(['./bower_components/vue/dist/vue.js'])
+    gulp.src(['./node_modules/vue/dist/vue.js'      //we dont need this as it will become part of vendor bundle
+                                                    //but copying it for some initial demo pages
+                                                    //we are also no longer shimming it
+        /*'./node_modules/vuetify/dist/vuetify.js'*/])
         .pipe(gulp.dest('./Scripts/'));
+    gulp.src(['./node_modules/vuetify/dist/vuetify.css', './node_modules/vuetify/dist/vuetify.css.map'])
+        .pipe(gulp.dest('./Content/'));
 
     //hello-module
     browserify('./Vue/hello-module.js', { debug: !production })
         .transform(babelify)
         .bundle()
         .pipe(fs.createWriteStream('./Scripts/hello-module.js'));
-    //hello-vue-component
-    browserify('./Vue/hello-vue-component.js', { debug: !production })
-        .transform(vueify)
-        .transform(babelify)
-        .transform(browserifyShim) //https://github.com/vuejs/vueify/issues/122, https://github.com/vuejs/vueify/issues/194
-        //.external('vue')
-        .bundle()
-        .pipe(fs.createWriteStream('./Scripts/hello-vue-component.js'));
-    //hello-router
-    browserify('./Vue/hello-router.js', { debug: !production })
-        .transform(vueify)
-        .transform(babelify)
-        .transform(browserifyShim)
-        //.external('vue')
-        .bundle()
-        .pipe(fs.createWriteStream('./Scripts/hello-router.js'));
-    //vee-validate
-    browserify('./Validation/vee-validate.js', { debug: !production })
-        .transform(vueify)
-        .transform(babelify)
-        .transform(browserifyShim)
-        .bundle()
-        .pipe(fs.createWriteStream('./Scripts/vee-validate.js'));
+
+    var b = browserify({ debug: !production });
+    vueVendors.forEach(lib => { b.require(lib); });
+    b.bundle()
+        .pipe(fs.createWriteStream('./Scripts/vue-bundle.js'));
+
+    var vueFiles = [{ folder: './Vue/', file: 'hello-vue-component.js' },
+        { folder: './Vue/', file: 'hello-router.js' },
+        { folder: './Validation/', file: 'vee-validate.js' },
+        { folder: './Vue/', file: 'hello-vuetify.js' }];
+    vueFiles.forEach(e => {
+        browserify(e.folder + e.file, { debug: !production })
+            .external(vueVendors)
+            .transform(vueify)
+            .transform(babelify)
+            .transform(browserifyShim) //https://github.com/vuejs/vueify/issues/122, https://github.com/vuejs/vueify/issues/194
+            .bundle()
+            .pipe(fs.createWriteStream('./Scripts/' + e.file));
+    });
 });
