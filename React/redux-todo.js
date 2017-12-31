6bead636f405b14887a1ca86306d75f53b42520a
khurram <khuziz@hotmail.com>
2018-01-01 02:51:34 +0500

    logger middleware

--- React/redux-todo.js
+++ React/redux-todo.js
@@ -1,11 +1,18 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import todoApp from './reducers/todo-reducers';
 import App from './components/todo-components';
 
-const store = createStore(todoApp);
+const logger = store => next => action => {
+    console.log('dispatching', action);
+    let result = next(action);
+    console.log('next state', store.getState());
+    return result;
+}
+
+const store = createStore(todoApp, applyMiddleware(logger));
 
 const render = () => ReactDOM.render(
     <Provider store={store}>
